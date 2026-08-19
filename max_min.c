5effb454abcfdb9054139754efaa5b905ea9ede4
Sujey-Sharoan-S <sujeyaj2004@gmail.com>
2026-08-19 20:15:22 +0530

    max and min in a array

--- max_min.c
+++ max_min.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int main()
+{
+    int i;
+    char ch[20] = {1,2,3,4,5};
+    int max = ch[0];
+    int min = ch[0];
+    for(i=1;i<5;i++)
+    {
+        if(ch[i]>max)
+        {
+        max=ch[i];
+        }
+        if(ch[i]<min)
+        {
+        min=ch[i];
+        }
+    }
+    printf("%d is the max value\n", max);
+    printf("\n%d is the smallest", min);
+    return 0;
+}
