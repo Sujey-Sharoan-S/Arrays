1ca4db51bcc03c62135a8f0558c6d2567de505f8
Sujey-Sharoan-S <sujeyaj2004@gmail.com>
2026-08-20 22:51:16 +0530

    find the dup

---
+++ duplicate.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    char num1[10] ={1,2,0,4};
+    char num2[10] ={10,0,30,40};
+    int i,j;
+    
+    for(i=0;i<4;i++)    
+    {
+        for(j=0;j<=4;j++)
+        {
+            if (num1[i]==num2[j])
+            {
+                printf("The duplicate Num is %d",num1[i]);     
+           }
+            
+            
+        }
+        
+    }
+    return 0;
+}
