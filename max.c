71a1a20169a3056a6492c519a3a1a0f9245a3d6a
Sujey-Sharoan-S <sujeyaj2004@gmail.com>
2026-08-27 00:08:16 +0530

    Max

---
+++ max.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int A[] = {8,4,5,6,9,10};
+    int temp,i,j;
+    int size = sizeof(A)/sizeof(A[0]);
+    for (i = 0; i < size; i++)
+    {
+        for(j = 0; j<size-1;j++)
+        if(A[j]>A[j+1])
+        {
+           temp = A[j];
+
+           A[j]= A[j+1];
+           A[j+1] = temp;
+        }
+        
+    }
+   
+    printf("\nMax is %d ",A[size-1]);
+    return 0;
+    
+
+}
