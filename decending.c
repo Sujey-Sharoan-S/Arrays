8c65cf6b9dcc9490ae67a4d4bfbc2efdfba3ac46
Sujey-Sharoan-S <sujeyaj2004@gmail.com>
2026-08-26 23:00:04 +0530

    Decending order

---
+++ decending.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int A[] = {8,4,5,6,9,10};
+    int temp,i,j;
+    int size = sizeof(A)/sizeof(A[0]);
+    for (i = 0; i < size; i++)
+    {
+        for(j = 0; j<size-1;j++)
+        if(A[j]<A[j+1])
+        {
+           temp = A[j];
+
+           A[j]= A[j+1];
+           A[j+1] = temp;
+        }
+    }
+    for(i=0;i<size;i++)
+    {
+        printf("%d ",A[i]);
+    }
+
+}
