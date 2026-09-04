009000dba09762b85104f1bf0cc915f00f746f4d
Sujey-Sharoan-S <sujeyaj2004@gmail.com>
2026-09-04 20:43:54 +0530

    Sum of lower triangle of the matrix

--- triangular_sum.c
+++ triangular_sum.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
     int A[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
@@ -8,6 +8,26 @@ int main()
         for(j=0;j<3;j++)
         {
             if(i<=j)
+            {
+                sum +=A[i][j];
+
+            }
+        }
+    } 
+    printf("Sum of the Upper Triangle elements in the matrix : %d" ,sum);
+    return 0;
+}
+*/
+#include<stdio.h>
+int main()
+{
+    int A[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int i,j,sum = 0;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(i>=j)
             {
                 sum +=A[i][j];
 
